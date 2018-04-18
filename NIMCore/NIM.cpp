53879b308e11f7d28e31e0008880750d2e9444d2
Seth <hklein@harding.edu>
2018-04-18 14:21:34 -0500

    AI 1.0
    Select AIMove should be working. No clue what would be the best way to return the information to the main program but it does select the winning move and stores it in AIMove in the prescribe notation.

--- NIMCore/NIM.cpp
+++ NIMCore/NIM.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <cmath>
 
 using namespace std;
 
 void makeMove(char*);
 void setupBoard();
 string getMove();
-void makeAIMove();
+char * selectAIMove(bitset<5> board[], int);
 
 void main() {
 
 	bool gameover = false;
 	string move;
 	char * cmove = new char[3];
+	char * aimove = new char[3];
 
+	bitset<5> board[9];
+	board[0] = 1;
+	board[1] = 4;
+	board[2] = 2;
+	
+	aimove = selectAIMove(board, 3);
 
-	setupBoard();
-
-	while (!gameover) {
-		move = getMove();
-		strcpy_s(cmove, 3, move.c_str());
-		makeMove();
-	}
+	cout << &aimove;
 }
 
 void setupBoard() {
@@ -31,8 +33,7 @@ void setupBoard() {
 
 	cout << "how many rows?";
 	cin >> numRows;
-	bitset<20> board[9];
-
+	bitset<5> board[9];
 	for (int i = 0; i < numRows; i++) {
 		cout << "how many stones in row " << i << "? ";
 		cin >> numStones;
@@ -59,20 +60,39 @@ void makeMove(char * move) {
 
 }
 
-void makeAIMove() {
-	bitset<20> board[20];
-	int numRows;
-	bitset<20> nimSum;
-	bool noMove = false;
+char* selectAIMove(bitset<5> board[], int numRows) {
+
+	bitset<5> nimSum;
+	bool noMove = true;
+	int moveAmount;
+	char AIMove[4];
+	char moveRow[2];
+	char cmoveAmount[3] = { '0' };
+	char temp[3];
 
 	for (int i = 0; i < numRows; i++) {
 		nimSum ^= board[i];
 	}
 
 	for (int i = 0; i < numRows && noMove; i++) {
-		bitset<20> target = nimSum ^ board[i];
-		if (target < board[i]) {
-
+		bitset<5> target = nimSum ^ board[i];
+		if (target.to_ulong() < board[i].to_ulong()) {
+			moveAmount = board[i].to_ulong() - target.to_ulong();
+			moveAmount = log2(moveAmount)+1;
+			noMove = false;
+			_itoa_s(i+1,moveRow,10);
 		}
 	}
+	_itoa_s(moveAmount, temp, 10);
+	if (moveAmount < 10) {
+		strncat_s(cmoveAmount, temp, 1);
+	}
+	else {
+		strcpy_s(cmoveAmount, temp);
+	}
+	
+	strcpy_s(AIMove, moveRow);
+	strncat_s(AIMove, cmoveAmount, 2);
+
+	return AIMove;
 }
